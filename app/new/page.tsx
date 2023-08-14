28ba14f09a14788c82d6d4fd0a50dd08493ca5ab
Brennan Colberg <brennan.colberg@gmail.com>
2023-08-13 20:16:16 -0700

    create quote with custom text

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,19 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 
 export default function AddQuote() {
-	async function addQuote(data) {
+	async function addQuote(data: FormData) {
 		"use server";
+		const text = data.get("text");
+		if (typeof text !== "string") return;
+
 		const prisma = new PrismaClient();
 		const quote = await prisma.quote.create({
-			data: {
-				text: "text",
-			},
+			data: { text },
 		});
 		return quote;
 	}
 
 	return (
 		<form action={addQuote}>
+			<textarea name="text" />
 			<button type="submit">Add Quote</button>
 		</form>
 	);
