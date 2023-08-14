91a78ed04d77127e2ffcab48fc1ebdbf1205bf9b
Brennan Colberg <brennan.colberg@gmail.com>
2023-08-13 21:01:40 -0700

    create authors inline

---
+++ app/api/persons/route.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const schema = z.object({
+	name: z.string(),
+});
+
+export async function POST(request: NextRequest) {
+	const body = await request.json();
+	const { name } = schema.parse(body);
+	const prisma = new PrismaClient();
+	const person = await prisma.person.create({
+		data: { name },
+	});
+	return NextResponse.json(person);
+}
--- app/new/NewQuoteForm.tsx
+++ app/new/NewQuoteForm.tsx
@@ -3,19 +3,21 @@
 import { Person } from "@prisma/client";
 import axios from "axios";
 import { useMemo, useState } from "react";
-import Select from "react-select";
+import CreatableSelect from "react-select/creatable";
 
-export default function NewQuoteForm({ persons }: { persons: Person[] }) {
-	const [text, setText] = useState("");
-
+export default function NewQuoteForm({
+	persons: initialPersons,
+}: {
+	persons: Person[];
+}) {
+	const [persons, setPersons] = useState<Person[]>(initialPersons);
 	const personOptions = useMemo(
-		() =>
-			persons.map((person) => ({
-				value: person.id,
-				label: person.name,
-			})),
+		() => persons.map((person) => ({ value: person.id, label: person.name })),
 		[persons]
 	);
+
+	const [text, setText] = useState("");
+
 	const [personId, setPersonId] = useState(null);
 
 	return (
@@ -36,13 +38,18 @@ export default function NewQuoteForm({ persons }: { persons: Person[] }) {
 			/>
 
 			{/* authors */}
-			<Select
+			<CreatableSelect
 				options={personOptions}
 				onChange={(newValue) => {
 					console.log(newValue);
 					setPersonId(newValue.value);
 				}}
 				value={personOptions.find((person) => person.value === personId)}
+				onCreateOption={async (inputValue) => {
+					const person = await axios.post("/api/persons", { name: inputValue });
+					setPersons((x) => [...x, person.data]);
+					setPersonId(person.data.id);
+				}}
 			/>
 
 			<button type="submit">Add Quote</button>
