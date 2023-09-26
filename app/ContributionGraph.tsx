cc53a02106513289cfdbbb95046b3d9f23fb2665
Brennan Colberg <brennan.colberg@gmail.com>
2023-09-26 08:46:42 -0700

    extend contribution graph through today

--- app/ContributionGraph.tsx
+++ app/ContributionGraph.tsx
@@ -4,6 +4,13 @@ import prisma from "@/prisma/prisma"
 import { Activity, Level } from "react-activity-calendar"
 import _ContributionGraph from "./_ContributionGraph"
 
+function localISODate(date: Date) {
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, "0")
+  const day = String(date.getDate()).padStart(2, "0")
+  return `${year}-${month}-${day}`
+}
+
 export default async function ContributionGraph() {
   const contributions = await prisma.quote.findMany({
     select: { createdAt: true },
@@ -11,21 +18,19 @@ export default async function ContributionGraph() {
 
   const countPerDate: { [key: string]: number } = {}
   let maxCount = 0
-  let firstDate = ""
-  let lastDate = ""
+
   contributions.forEach((contribution) => {
     const date = new Date(contribution.createdAt)
     // get ISO-format date according to local timezone
     // in order to bucket contributions by date
-    const year = date.getFullYear()
-    const month = String(date.getMonth() + 1).padStart(2, "0")
-    const day = String(date.getDate()).padStart(2, "0")
-    const dateStr = `${year}-${month}-${day}`
+    const dateStr = localISODate(date)
     countPerDate[dateStr] = (countPerDate[dateStr] ?? 0) + 1
-    firstDate = !firstDate || dateStr > firstDate ? dateStr : firstDate
-    lastDate = !lastDate || dateStr < lastDate ? dateStr : lastDate
     maxCount = Math.max(maxCount, countPerDate[dateStr])
   })
+
+  // add today if not present
+  const today = localISODate(new Date())
+  if (!(today in countPerDate)) countPerDate[today] = 0
 
   const data: Activity[] = Object.entries(countPerDate).map(
     ([date, count]) => ({
