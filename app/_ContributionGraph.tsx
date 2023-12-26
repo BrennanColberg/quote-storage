22d78422a71926b94b5c9fea5beb6d04031cee59
Brennan Colberg <brennan.colberg@gmail.com>
2023-12-25 20:38:00 -0600

    light mode on github chart

--- app/_ContributionGraph.tsx
+++ app/_ContributionGraph.tsx
@@ -2,5 +2,12 @@
 import ActivityCalendar, { Activity } from "react-activity-calendar"
 
 export default function _ContributionGraph({ data }: { data: Activity[] }) {
-  return <ActivityCalendar data={data} hideTotalCount hideColorLegend />
+  return (
+    <ActivityCalendar
+      data={data}
+      hideTotalCount
+      hideColorLegend
+      colorScheme="light"
+    />
+  )
 }
